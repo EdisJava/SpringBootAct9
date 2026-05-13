5a1c0ac8ea14f7cb081fdfb68a2d241bae180836
Edgar Javier Quesada Morales <edgarjqm122@gmail.com>
2026-05-13 17:01:46 +0100

    Error arreglado

--- src/test/java/org/miapp/service/VehiculoServiceTest.java
+++ src/test/java/org/miapp/service/VehiculoServiceTest.java
@@ -33,7 +33,7 @@ public class VehiculoServiceTest {
         vehiculo.setId(1L);
         vehiculo.setMarca("Toyota");
         vehiculo.setModelo("Corolla");
-        vehiculo.setAnio("w");
+        vehiculo.setAnio(2020);
         vehiculo.setColor("Blanco");
         vehiculo.setMatricula("ABC123");
 
